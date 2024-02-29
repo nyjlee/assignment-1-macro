a51cafa2bd846f4b47e7a6604527f0149feb0ae9
Giani Ranzetti <gianiranzetti1@gmail.com>
2024-02-29 19:37:10 +0000

    VAR attempt

--- Giani.py
+++ Giani.py
@@ -4,9 +4,8 @@ import pandas as pd
 # Read the Excel file into a DataFrame
 df = pd.read_excel("Macrodata.xlsx")
 
-# Format the 'Period' column as DD-MM-YYYY and set the 'Period' column as the index
+# Convert the 'Period' column to datetime format and set it as the index
 df['Period'] = pd.to_datetime(df['Period'])
-df['Period'] = df['Period'].dt.strftime('%d-%m-%Y')
 df.set_index('Period', inplace=True)
 
 #Set up variables
@@ -21,12 +20,18 @@ rex = df['EUR/USD']
 plt.figure(figsize=(10, 6))
 plt.plot(df.index, inf, label='CPI Domestic')
 plt.plot(df.index, unemp, label='Unemployment')
-plt.plot(df.index, int, label='Domestic ST Interest Rate')
+plt.plot(df.index, int, label='Domestic Short-Term Interest Rate')
+
+# Set ticks at every 2 years
+plt.xticks(df.index[::8], df.index.strftime('%d-%m-%Y')[::8], rotation=45)
 
 # Set labels and title
 plt.xlabel('Date')
-plt.ylabel('Values')
-plt.title('Time Series of CPI Domestic, Unemployment, and Domestic ST Interest Rate')
+plt.ylabel('Rates (%)')
+plt.title('Time Series of CPI Domestic, Unemployment, and Domestic Short-Term Interest Rate')
 plt.legend()
 plt.show()
 
+data = pd.DataFrame({'CPI Domestic': inf, 'CPI Foreign': inf_f, 'Unemployment': unemp,
+                     'Domestic ST interest rate': int, 'Foreign ST interest rate': int_f,
+                     'EUR/USD': rex})
